d5eacfd6f87f2db588a34181ce4e0dfb93a10f6a
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-09-04 00:38:34 -0500

    index.js y componente PrimeraApp

---
+++ src/PrimeraApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+    return (
+        <>
+            <h1>Hola React!</h1>
+            <p>Aprendiento React...</p>
+        </>
+    );
+}
+export default PrimeraApp;
